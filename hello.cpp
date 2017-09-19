4f199fdf22bb4d1f9c0eccde9007eb9aa531c3fe
jamylj <32109236+jamylj@users.noreply.github.com>
2017-09-19 14:21:35 -0700

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,10 @@
 #include <iostream>  
 using namespace std; 
-
+//I added my full name
 	
 int main()
 {
 	cout << "Hello!" <<  endl;
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
-	cout << "I can count to potato and spell 14 numerically" << endl;
+	cout << "Jamyl Johnson" << endl;
 }
